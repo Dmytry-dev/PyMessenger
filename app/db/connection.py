aa0767d11e30768bb4fbaa0954f51454611abd65
Dmytry-dev <dmytrydev@gmail.com>
2025-08-15 19:24:38 +0200

    database connection

--- app/db/connection.py
+++ app/db/connection.py
@@ -1,8 +1,50 @@
-import sqlalchemy
-from sqlalchemy.ext.asyncio import create_async_engine
-import asyncpg
-from dotenv import load_dotenv
-import os
+'''
+Dmytry-dev
+PyChat
+Connection to Database
+15.08.25
+'''
 
-load_dotenv(".env")
-create_async_engine(os.environ.get("DATABASE_URL"))
+import asyncpg
+import asyncio
+import os
+from dotenv import load_dotenv
+
+DEBUG = True
+
+load_dotenv()
+
+user = os.getenv("user")
+host = os.getenv("host")
+database = os.getenv("database")
+password = os.getenv("password")
+
+if(DEBUG == True):
+    print(user, host, database, password)
+    
+async def connection(user, host, database, password):
+
+    try:
+        connection = await asyncpg.connect(
+            user = user,
+            host = host,
+            database = database,
+            password = password
+        )
+
+        if(DEBUG == True):
+            result = await connection.fetchval("SELECT 1;")
+            print("DB test", result)
+
+    except Exception as ex:
+        print("Mistake: ", ex)
+    finally:
+        if connection:
+            await connection.close()
+            
+            if(DEBUG == True):
+                print("Connection closed")
+            
+
+asyncio.run(connection(user, host, database, password))
+    
